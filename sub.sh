6af8dea4f5aa6801ebb08716b1765bbbe073aff9
lucruzz <62730384+lucruzz@users.noreply.github.com>
2020-05-24 13:22:37 -0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1               #Numero de Nós
+#SBATCH --ntasks-per-node=1     #Numero de tarefas por Nó
+#SBATCH --ntasks=1              #Numero de tarefas
+#SBATCH -p cpu_dev              #Fila (partition) a ser utilizada
+#SBATCH -J JOB_BEAST            #Nome job
+
+#Carregar os módulos
 module load python/3.8.2
+#pip3 install --user parsl
 module load beagle/current
 module load beast/1.10
+module load java/jdk-8u201
 
-python3 beast.py
+#Acessar o diretório onde o script está localizado
+cd /scratch/cenapadrjsd/lucas.silva/beast
+
+EXEC=/scratch/cenapadrjsd/lucas.silva/beast/beast.py
+
+#Executa o script
+time python3 $EXEC
